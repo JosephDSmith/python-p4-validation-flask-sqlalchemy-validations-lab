85318ff9e5ef8bd9aa62e147528b8cd492bbc4f9
Joseph Smith <joesmith40@gmail.com>
2023-06-22 19:42:49 -0500

    completed lab

--- server/models.py
+++ server/models.py
@@ -12,6 +12,18 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+
+    @validates('name')
+    def validate_name(self, key, name_value):
+        if name_value == '':
+            raise ValueError("Name must be a string")
+        return name_value
+    @validates('phone_number')
+    def validate_phone(self, key, phone_value):
+        if len(phone_value) < 10:
+            raise ValueError("Phone number must be 10 digits long")
+        return phone_value
+
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
@@ -26,6 +38,32 @@ class Post(db.Model):
     summary = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    @validates('title')
+    def validates_title(self, key, title_value):
+        if title_value == '':
+            raise ValueError("Title must be a string")
+        elif "Won't Believe" not in title_value and "Secret" not in title_value and "Top" not in title_value and "Guess" not in title_value:
+            raise ValueError("Needs to be more clickbaity!")
+        return title_value
+    
+    @validates('content')
+    def validates_content(self, key, content):
+        if len(content) <= 249:
+            raise ValueError("Content too short test. Less than 250 chars.")
+        return content
+    
+    @validates('summary')
+    def validates_summary(self, key, summary):
+        if len(summary) >= 250:
+            raise ValueError("Summary too long test. More than 250 chars.")
+        return summary
+    
+    @validates('category')
+    def validates_category(self, key, category):
+        if category != "Fiction" and category != "Non-Fiction":
+            raise ValueError('Incorrect Category')
+        return category
 
 
     def __repr__(self):
